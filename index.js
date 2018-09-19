417d42e1aa32e70091e2f307d35b515d0e2a4ac2
Tharaka Romesh <tharakaromesh@gmail.com>
2018-09-19 12:02:31 +0530

    Add Audio extraction, Video compression, Overlay image options

--- index.js
+++ index.js
@@ -44,6 +44,77 @@ const ffmpegjs = {
         }
       );
     });
+  },
+
+  extractaudio: (input, output) => {
+    return new Promise(function(resolve, reject) {
+      exec(
+        `ffmpeg -hide_banner -loglevel quiet -i ${input} -f mp3 -ab 192000 -vn ${output}.mp3`,
+        (error, stdout, stderr) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+          resolve(stdout);
+        }
+      );
+    });
+  },
+  compressvideo: (input, output) => {
+    return new Promise(function(resolve, reject) {
+      exec(
+        `ffmpeg -hide_banner -loglevel quiet -i ${input} -vcodec h264 -acodec mp3 ${output}.mp3`,
+        (error, stdout, stderr) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+          resolve(stdout);
+        }
+      );
+    });
+  },
+  compressvideo: (input, output) => {
+    return new Promise(function(resolve, reject) {
+      exec(
+        `ffmpeg -hide_banner -loglevel quiet -i ${input} -vcodec h264 -acodec mp3 ${output}.mp3`,
+        (error, stdout, stderr) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+          resolve(stdout);
+        }
+      );
+    });
+  },
+  addoverlayimagetime: (input1, input2, start, end, x, y, output) => {
+    return new Promise(function(resolve, reject) {
+      exec(
+        `ffmpeg -hide_banner -loglevel quiet -i ${input1} -i ${input2} -filter_complex "[0:v][1:v] overlay=${x}:${y}:enable='between(t,${start},${end})'" -pix_fmt yuv420p -c:a copy ${output}.mp4`,
+        (error, stdout, stderr) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+          resolve(stdout);
+        }
+      );
+    });
+  },
+  addoverlayimage: (input1, input2, x, y, output) => {
+    return new Promise(function(resolve, reject) {
+      exec(
+        `ffmpeg -hide_banner -loglevel quiet -i ${input1} -i ${input2} -filter_complex "[0:v][1:v] overlay=${x}:${y}" -codec:a copy ${output}.mp4`,
+        (error, stdout, stderr) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+          resolve(stdout);
+        }
+      );
+    });
   }
 };
 
--- readme.md
+++ readme.md
@@ -7,3 +7,6 @@ Coming Soon!
 - [x] Video Conversion
 - [x] Thumbnail Generation
 - [x] Image Capturing
+- [x] Audio Extraction
+- [x] Video Compression
+- [x] Overlay Image
